42031fde337d21c832c45595b19afd45fa7cefe8
Zenobius Jiricek <zeno@zibmedia.com.au>
2013-05-03 17:35:02 +0930

    emit signal when saving, so other apps can deal with permissions in guardian

--- puraibeto/conf.py
+++ puraibeto/conf.py
@@ -1,5 +1,12 @@
 from django.conf import settings
 from appconf import AppConf
 
+
 class PuraibetoConf(AppConf):
     BACKEND = "basic"
+    PERMISSION_CANDOWNLOAD = 'download_privatefile'
+    PERMISSION_CANCREATE = 'add_privatefile'
+    PERMISSION_CANVIEW = 'view_privatefile'
+    PERMISSION_CANDELETE = 'delete_privatefile'
+    PERMISSION_CANCHANGE = 'change_privatefile'
+    REGISTER_ADMIN = False
