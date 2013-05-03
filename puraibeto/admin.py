f40be9b247664ee22e3dfd6b097629a280764c8b
Zenobius Jiricek <zeno@zibmedia.com.au>
2013-05-03 17:34:03 +0930

    don't register admin by default

--- puraibeto/admin.py
+++ puraibeto/admin.py
@@ -1,14 +1,11 @@
 from django.contrib import admin
-from django.core.urlresolvers import reverse, reverse_lazy
 
 from guardian.admin import GuardedModelAdmin
 
 from . import models
 from .conf import settings
 
-
-class PrivateFileAdmin(GuardedModelAdmin):
-
-    list_display = [ 'name', 'file', 'attached_to' ]
-
-admin.site.register(models.PrivateFile, PrivateFileAdmin)
+if settings.PURAIBETO_REGISTER_ADMIN:
+    class PrivateFileAdmin(GuardedModelAdmin):
+        list_display = ['name', 'file', 'attached_to']
+    admin.site.register(models.PrivateFile, PrivateFileAdmin)
